684139529e19c3b108c320fdb59cbaed789917f8
ProphetLamb <prophet.lamb@gmail.com>
2022-05-24 09:46:01 +0200

    Remove c# template Use cargo workspaces

--- scripts/tooling.ps1
+++
@@ -1,3 +0,0 @@
-& choco install dotnet-sdk --version 6.0.101
-& dotnet tool install --global coveralls.net
-& dotnet tool install -g XMLDoc2Markdown
--- src/AssemblyInfo.cs
+++
@@ -1,2 +0,0 @@
-[assembly: System.CLSCompliant(true)]
-namespace CrossoutLogInfo;
